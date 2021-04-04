fff2b023317adcbc80d69a477d4216d70695002d
RayDarar <dfqgth400@gmail.com>
2021-04-05 02:39:29 +0600

    work(post-likes)

--- Scripts/procedures/p_set_like.sql
+++ Scripts/procedures/p_set_like.sql
@@ -3,12 +3,28 @@ create or replace procedure p_set_like(_post_id UUID, _user_id UUID)
 as
 $$
 declare
-	_contains boolean;
+	_contains boolean = false;
 begin
-	select _user_id = any(p."content"->>'likes'::UUID[]) into _contains from posts p
+	select p."content"->'likes' @> jsonb_build_array(_user_id) into _contains from posts p
 	where p.id = _post_id;
 
-	raise notice 'Value %', _contains;
+	if _contains = true then
+		update posts
+		set "content" = jsonb_set(
+			"content"::jsonb,
+			array['likes'],
+			("content"->'likes')::jsonb - _user_id::text
+		)
+		where id = _post_id;
+	else
+		update posts
+		set "content" = jsonb_set(
+			"content"::jsonb,
+			array['likes'],
+			("content"->'likes')::jsonb || jsonb_build_array(_user_id)::jsonb
+		) 
+		where id = _post_id;
+	end if;
 end
 $$
 
