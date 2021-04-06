62f4809272be2790ff33613365b1d88b3c13b18e
RayDarar <dfqgth400@gmail.com>
2021-04-07 05:43:39 +0600

    work(friend-recommendations)

--- Scripts/functions/f_get_user_recommendations.sql
+++ Scripts/functions/f_get_user_recommendations.sql
@@ -1,27 +1,42 @@
 -- getting friends of friends
 create or replace function f_get_user_recommendations(_user_id UUID)
-	returns setof users
+	returns table (
+		id UUID,
+		first_name varchar,
+		last_name varchar,
+		username varchar,
+		avatar varchar
+	)
 	language plpgsql
 as
 $$
 declare
+	_ids UUID[];
 begin
+	_ids = array(
+		select f.friend_id from friends f
+		where f.user_id in 
+			(
+				select f1.friend_id from friends f1
+				where f1.user_id = _user_id
+			)
+		and f.friend_id <> _user_id 
+	);
+
 	return query
-	select * from users
-	where role = 'user'
-	and status = 'enabled'
-	and id in 
-		(
-			select friend_id from friends
-			where user_id in 
-				(
-					select friend_id from friends
-					where user_id = _user_id
-				)
-			and friend_id <> _user_id
-		)
+	select 
+		u.id,
+		u.first_name,
+		u.last_name,
+		u.username,
+		(select r."path" from resources r where r.id = u.avatar) as avatar
+	from users u
+	where u."role" = 'user'
+		and u.status = 'enabled'
+		and u.id = any(_ids)
+		or cardinality(_ids) = 0
 	order by random()
-	limit 10;
+	limit 5;
 end
 $$
 
