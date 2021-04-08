6625b379af3d4ab0534b07514f3b200eda45347d
RayDarar <dfqgth400@gmail.com>
2021-04-09 02:08:57 +0600

    work(communities)

--- Scripts/functions/f_search_communities.sql
+++ Scripts/functions/f_search_communities.sql
@@ -2,14 +2,38 @@ create or replace function f_search_communities(
 		_title varchar, 
 		_description varchar
 		)
-	returns setof communities
+	returns table (
+		id UUID,
+		title varchar,
+		main_image varchar,
+		description varchar,
+		admin_id UUID,
+		status GENERIC_STATUS_ENUM,
+		subscribers bigint
+	)
 	language plpgsql
 as
 $$
+declare
+	_limit int = null;
 begin
+	if _title is null and _description is null then
+		_limit = 1000;
+	end if;
+	
 	return query
-	select * from communities
-	where (position(_title in title) not in (0) or _title is null)
-	and (position(_description in description) not in (0) or _description is null);
+	select 
+		c.id,
+		c.title,
+		(select r."path" from resources r where r.id = c.main_image) as main_image,
+		c.description,
+		c.admin_id,
+		c.status,
+		(select count(*) from subscriptions s where s.community_id = c.id) as subscribers
+	from communities c
+	where f_str_search(c.title, _title)
+		and f_str_search(c.description, _description)
+		and c.status = 'enabled'
+	limit _limit;
 end
 $$
